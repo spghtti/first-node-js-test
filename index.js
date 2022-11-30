b1d537957742e66fbdd38b1b249b50b440e74d34
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2022-11-30 14:44:24 -0500

    fixed URL pathnames to not have file extension

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const url = require('url');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+  // Hardcoding the .html filepath prevents the .css stylesheet loading
   const filePath = path.join(
     __dirname,
     'public',
-    req.url === '/' ? 'index.html' : req.url
+    req.url === '/' ? 'index.html' : `${req.url}.html`
   );
 
-  //   const myURL = new URL(`/${}`)
-
   const extension = path.extname(filePath);
+
   let contentType = 'text/html';
   switch (extension) {
     case '.js':
@@ -30,6 +30,25 @@ const server = http.createServer((req, res) => {
       break;
   }
 
+  // fs.readFile(filePath, (err, content) => {
+  //   if (err) {
+  //     if (err.code == 'ENOENT') {
+  //       fs.readFile(
+  //         path.join(__dirname, 'public', '404.html'),
+  //         (err, content) => {
+  //           res.writeHead(200, { 'Content-Type': 'text/html' });
+  //           res.end(content, 'utf8');
+  //         }
+  //       );
+  //     } else {
+  //       res.writeHead(500);
+  //       res.end(`Server error: ${err.code}`);
+  //     }
+  //   } else {
+  //     res.writeHead(200, { 'Content-Type': contentType });
+  //     res.end(content, 'utf8');
+  //   }
+  // });
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code == 'ENOENT') {
