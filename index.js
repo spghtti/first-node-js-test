1f1fb9a4e88e3c5bfa018d2760ac1a71ce10e62a
Matt Daniele <2266075+spghtti@users.noreply.github.com>
2022-12-07 11:40:21 -0500

    Refactored using Express

--- index.js
+++ index.js
@@ -1,77 +1,22 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 const path = require('path');
-const url = require('url');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  // Hardcoding the .html filepath prevents the .css stylesheet loading
-  const filePath = path.join(
-    __dirname,
-    'public',
-    req.url === '/' ? 'index.html' : `${req.url}.html`
-  );
-
-  const extension = path.extname(filePath);
-
-  let contentType = 'text/html';
-  switch (extension) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-    case '.js':
-      contentType = 'text/css';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.jpg':
-      contentType = 'text/jpg';
-      break;
-  }
-
-  // fs.readFile(filePath, (err, content) => {
-  //   if (err) {
-  //     if (err.code == 'ENOENT') {
-  //       fs.readFile(
-  //         path.join(__dirname, 'public', '404.html'),
-  //         (err, content) => {
-  //           res.writeHead(200, { 'Content-Type': 'text/html' });
-  //           res.end(content, 'utf8');
-  //         }
-  //       );
-  //     } else {
-  //       res.writeHead(500);
-  //       res.end(`Server error: ${err.code}`);
-  //     }
-  //   } else {
-  //     res.writeHead(200, { 'Content-Type': contentType });
-  //     res.end(content, 'utf8');
-  //   }
-  // });
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == 'ENOENT') {
-        fs.readFile(
-          path.join(__dirname, 'public', '404.html'),
-          (err, content) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf8');
-          }
-        );
-      } else {
-        res.writeHead(500);
-        res.end(`Server error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf8');
-    }
-  });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/about.html'));
 });
+
+app.get('/contact', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/contact-me.html'));
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public/404.html'));
+});
+
+app.listen(3000);
